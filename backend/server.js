9fea2040ee9aa20f2bfac42ce87a7946c57b3a39
Palak Kalra <palakkalra357@gmail.com>
2026-03-29 02:53:11 +0530

    fixed DB connection

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,13 @@ app.use(cors());
 app.use(express.json());
 
 // ================= DB CONNECTION =================
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "trello_clone",
-  port: 3307,
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
