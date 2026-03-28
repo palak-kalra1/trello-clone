9c023dee03b00dd28dbef1dedaa2d09154ddd577
Palak Kalra <palakkalra357@gmail.com>
2026-03-29 04:37:35 +0530

    use MYSQL_URL for DB

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,7 @@ app.use(express.json());
 
 // ================= DB CONNECTION =================
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: "railway",
-  port: process.env.DB_PORT,
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect((err) => {
   if (err) {
